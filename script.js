437854a313c53b3d9d48267f8a06554a618bb6d9
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-01-16 05:22:33 +0400

    add objects for gameBoard and factory function to create players

---
+++ script.js
@@ -0,0 +1,37 @@
+const gameboard = (function () {
+  let totalScore = {
+    firstPlayer: 0,
+    secondPlayer: 0,
+  };
+  let gamefield = ["", "", "", "", "", "", "", "", ""];
+
+  function displayBoard() {
+    console.log(
+      `[${gamefield[0]}] [${gamefield[1]}] [${gamefield[2]}] \n[${gamefield[3]}] [${gamefield[4]}] [${gamefield[5]}] \n[${gamefield[6]}] [${gamefield[7]}] [${gamefield[8]}]`
+    );
+  }
+
+  function displayTotalScore() {
+    console.log(
+      `Total score is ${totalScore.firstPlayer}:${totalScore.secondPlayer}`
+    );
+  }
+  return { displayBoard, displayTotalScore };
+})();
+
+function createPlayer(name) {
+  let playerName = name;
+  let victories = 0;
+
+  function incrementVictories() {
+    ++victories;
+  }
+
+  function greetings() {
+    console.log(`Hi, I'm ${playerName} and I've got ${victories} already`);
+  }
+
+  return { name, incrementVictories, greetings};
+};
+
+
