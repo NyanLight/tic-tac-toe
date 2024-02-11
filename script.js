bbeb1c204f061c90b39a26c37e58f2030c1d933a
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-02-11 21:41:13 +0400

    buttons disable when someone wins now

--- script.js
+++ script.js
@@ -30,17 +30,14 @@ const gameboard = (function () {
 })();
 
 function createPlayer(name) {
-  let victories = 0; 
+  let victories = 0;
   const addVictory = () => victories++;
   const getVicrories = () => victories;
 
-  return { name, getVicrories, addVictory};
+  return { name, getVicrories, addVictory };
 }
 
-const gameController = function (
-  playerOne,
-  playerTwo 
-) {
+const gameController = function (playerOne, playerTwo) {
   const marks = ["x", "o"];
   const markDistribution = function () {
     if (Math.random() >= 0.5) {
@@ -59,17 +56,16 @@ const gameController = function (
   const switchActivePlayer = function () {
     activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
   };
-  const getActivePlayer = () => activePlayer.name; 
+  const getActivePlayer = () => activePlayer.name;
 
   const printNewRound = function () {
     gameboard.displayField();
     console.log(`${activePlayer.name}, it's your turn!`);
   };
 
-  
   const checkWinner = function () {
     let rows, columns, diagonal, anti;
-    
+
     for (let i = 0; i < gameboard.getGamefield().length; i++) {
       let row = 0;
       for (let j = 0; j < gameboard.getGamefield()[i].length; j++) {
@@ -80,7 +76,7 @@ const gameController = function (
         break;
       }
     }
-    
+
     for (j = 0; j < gameboard.getGamefield().length; j++) {
       let column = 0;
       for (i = 0; i < gameboard.getGamefield().length; i++) {
@@ -91,53 +87,55 @@ const gameController = function (
         break;
       }
     }
-    
+
     let diagonalCounter = 0;
     for (i = 0; i < gameboard.getGamefield().length; i++) {
       if (gameboard.getGamefield()[i][i] === activePlayer.mark)
-      diagonalCounter++;
-  }
-  if (diagonalCounter === 3) diagonal = true;
-  
-  let antiCounter = 0;
-  j = 2;
-  for (i = 0; i < gameboard.getGamefield().length; i++) {
-    if (gameboard.getGamefield()[i][j] === activePlayer.mark) antiCounter++;
-    j--;
-  }
-  if (antiCounter === 3) anti = true;
-  
-  if (rows === true || columns === true || diagonal === true || anti === true)
-  return true;
-};
+        diagonalCounter++;
+    }
+    if (diagonalCounter === 3) diagonal = true;
 
-let turnCounter = 0;
-const tieCheck = function () {
-  if (turnCounter === 9) return true;
-};
+    let antiCounter = 0;
+    j = 2;
+    for (i = 0; i < gameboard.getGamefield().length; i++) {
+      if (gameboard.getGamefield()[i][j] === activePlayer.mark) antiCounter++;
+      j--;
+    }
+    if (antiCounter === 3) anti = true;
 
-const restartGame = function () {
-  markDistribution();
-  activePlayer = playerOne.mark === marks[0] ? playerOne : playerTwo;
-  gameboard.cleanField();
-  turnCounter = 0;
-};
+    if (rows === true || columns === true || diagonal === true || anti === true)
+      return true;
+  };
 
-const playRound = (row, column) => {
-  console.log(
-    `${activePlayer.name}'s turn is row ${row} and column ${column}`
+  let turnCounter = 0;
+  const tieCheck = function () {
+    if (turnCounter === 9) return true;
+  };
+
+  const restartGame = function () {
+    markDistribution();
+    activePlayer = playerOne.mark === marks[0] ? playerOne : playerTwo;
+    gameboard.cleanField();
+    turnCounter = 0;
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `${activePlayer.name}'s turn is row ${row} and column ${column}`
     );
     gameboard.makeTurn(row, column, (mark = activePlayer.mark));
     turnCounter++;
     if (checkWinner()) {
-      (activePlayer === playerOne)? playerOne.addVictory(): playerTwo.addVictory();
+      activePlayer === playerOne
+        ? playerOne.addVictory()
+        : playerTwo.addVictory();
       printNewRound();
       console.log(`${activePlayer.name} is a winner! Congratulations.`);
-      return 'win';
+      return "win";
     } else if (tieCheck()) {
       printNewRound();
       console.log(`It's a tie game.`);
-      return 'tie';
+      return "tie";
     } else {
       switchActivePlayer();
       printNewRound();
@@ -146,53 +144,79 @@ const playRound = (row, column) => {
 
   printNewRound();
 
-  return { playRound, getActivePlayer, restartGame};
+  return { playRound, getActivePlayer, restartGame };
 };
 
 function screenController() {
-  const playerOne = createPlayer('First');
-  const playerTwo = createPlayer('Second');
+  const playerOne = createPlayer("First");
+  const playerTwo = createPlayer("Second");
   const game = gameController(playerOne, playerTwo);
 
   const turnDiv = document.getElementById("turn");
   const boardDiv = document.getElementById("board");
-  const restartBtn = document.getElementById('restartBtn');
+  const restartBtn = document.getElementById("restartBtn");
+  const resultDiv = document.getElementById("result");
 
   function updateScreen() {
     turnDiv.textContent = `${game.getActivePlayer()}, it's your turn!`;
-    boardDiv.textContent = '';
+    boardDiv.textContent = "";
 
     gameboard.getGamefield().forEach((row, rowIndex) => {
       row.forEach((cell, columnIndex) => {
-        const cellButton = document.createElement('button');
-        cellButton.classList.add('cell');
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
         cellButton.dataset.column = columnIndex;
         cellButton.dataset.row = rowIndex;
-        cellButton.textContent = gameboard.getGamefield()[rowIndex][columnIndex];
-        boardDiv.appendChild(cellButton); 
-      }
-      );
+        cellButton.textContent =
+          gameboard.getGamefield()[rowIndex][columnIndex];
+        boardDiv.appendChild(cellButton);
+      });
     });
-  };
+  }
+
+  function disableButtons() {
+    const cells = document.getElementsByClassName("cell");
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].disabled = true;
+    }
+  }
 
   function clickHandler(e) {
     const clickedRow = e.target.dataset.row;
     const clickerColumn = e.target.dataset.column;
-    if (!e.target.dataset.row || !e.target.dataset.column || e.target.textContent !== '-') return; 
+    if (
+      !e.target.dataset.row ||
+      !e.target.dataset.column ||
+      e.target.textContent !== "-"
+    )
+      return;
 
-    game.playRound(clickedRow, clickerColumn);
-    updateScreen(); 
+    const turn = game.playRound(clickedRow, clickerColumn);
+    if (turn === "win") {
+      updateScreen();
+      resultDiv.textContent = `${
+        game.getActivePlayer().name
+      } is a winner. Congratulations!`;
+      disableButtons();
+    } else if (turn === "tie") {
+      updateScreen();
+      resultDiv.textContent = `It's a tie game! Try again`;
+      disableButtons();
+    } else {
+      updateScreen();
+    }
   }
 
-  function restartHandler () {
+  function restartHandler() {
     game.restartGame();
     updateScreen();
+    resultDiv.textContent = '';
   }
 
-  restartBtn.addEventListener('click', restartHandler); 
+  restartBtn.addEventListener("click", restartHandler);
   boardDiv.addEventListener("click", clickHandler);
 
   updateScreen();
-};
+}
 
 screenController();
